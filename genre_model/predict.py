9c4fac66b42e465b1ec2dfb354c3f0f73b7a371d
Petr Doroshev <doroshevpetr@gmail.com>
2025-06-06 18:05:04 +0300

    predicition added

--- genre_model/predict.py
+++ genre_model/predict.py
@@ -1,32 +1,37 @@
 import typing as t
-
 import pandas as pd
-
 from genre_model import __version__ as _version
-from genre_model.config.core import config
 from genre_model.processing.data_manager import load_pipeline
 from genre_model.processing.validation import validate_inputs
+from genre_model.processing.feature_extraction import extract_features
+from librosa import load
+from config.core import config
 
 pipeline_file_name = f"{config.app_config.pipeline_save_file}{_version}.pkl"
-_price_pipe = load_pipeline(file_name=pipeline_file_name)
+pipeline = load_pipeline(file_name=pipeline_file_name)
 
 
 def make_prediction(
     *,
     input_data: t.Union[pd.DataFrame, dict],
 ) -> dict:
-    """Make a prediction using a saved model pipeline."""
 
     data = pd.DataFrame(input_data)
-    validated_data, errors = validate_inputs(input_data=data)
+    #validated_data, errors = validate_inputs(input_data=data)
 
-    results: t.Dict[str, t.Any] = {"preds": None, "probs": None, "version": _version, "errors": errors}
+    results: t.Dict[str, t.Any] = {"preds": None, "probs": None, "version": _version}
 
-    preds = _price_pipe.predict(X=validated_data[config.ml_model_config.features])
-    probs = _price_pipe.predict_proba(X=validated_data[config.ml_model_config.features])[:, 1]
+    preds = pipeline.predict(X=data[config.ml_model_config.features])
+    probs = pipeline.predict_proba(X=data[config.ml_model_config.features])
 
-    # Fill the results dict
     results["preds"] = [pred for pred in preds]
     results["probs"] = [prob for prob in probs]
 
     return results
+
+
+if __name__ == "__main__":
+
+    audio, sr = load("../\GTA_ Vice City - Wildstyle  Hashim - Al Naafiysh (The Soul).mp3")
+    features_data = extract_features(audio, sr)
+    print(make_prediction(input_data=features_data))
