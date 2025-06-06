985409c776cefa7320dc0274bc1753c2ac13846f
Petr Doroshev <doroshevpetr@gmail.com>
2025-06-06 23:53:38 +0300

    validation added

--- genre_model/predict.py
+++ genre_model/predict.py
@@ -1,5 +1,6 @@
 import typing as t
 import pandas as pd
+import numpy as np
 from genre_model import __version__ as _version
 from genre_model.processing.data_manager import load_pipeline
 from genre_model.processing.validation import validate_inputs
@@ -14,24 +15,27 @@ pipeline = load_pipeline(file_name=pipeline_file_name)
 def make_prediction(
     *,
     input_data: t.Union[pd.DataFrame, dict],
-) -> dict:
+) -> t.Tuple[dict, t.Optional[dict]]:
 
     data = pd.DataFrame(input_data)
-    #validated_data, errors = validate_inputs(input_data=data)
+    validated_data, errors = validate_inputs(input_data=data)
 
     results: t.Dict[str, t.Any] = {"preds": None, "probs": None, "version": _version}
 
-    preds = pipeline.predict(X=data[config.ml_model_config.features])
-    probs = pipeline.predict_proba(X=data[config.ml_model_config.features])
+    if not errors:
 
-    results["preds"] = [pred for pred in preds]
-    results["probs"] = [prob for prob in probs]
+        preds = pipeline.predict(X=validated_data[config.ml_model_config.features])
+        probs = pipeline.predict_proba(X=validated_data[config.ml_model_config.features])
 
-    return results
+        results["preds"] = [pred for pred in preds]
+        results["probs"] = [prob for prob in probs]
+
+    return results, errors
 
 
 if __name__ == "__main__":
 
     audio, sr = load("../GTA_ Vice City - Wildstyle  Hashim - Al Naafiysh (The Soul).mp3")
     features_data = extract_features(audio, sr)
+    features_data["chroma_stft_mean"] = [np.nan]
     print(make_prediction(input_data=features_data))
--- genre_model/processing/validation.py
+++ genre_model/processing/validation.py
@@ -1,90 +1,85 @@
 import re
 from typing import List, Optional, Tuple
-
 import numpy as np
 import pandas as pd
 from pydantic import BaseModel, ValidationError
-
 from genre_model.config.core import config
-
-
-# retain only the first cabin if more than
-# 1 are available per passenger
-def get_first_cabin(row):
-    try:
-        return row.split()[0]
-    except:
-        return np.nan
-
-
-def get_title(passenger):
-    line = passenger
-    if re.search("Mrs", line):
-        return "Mrs"
-    elif re.search("Mr", line):
-        return "Mr"
-    elif re.search("Miss", line):
-        return "Miss"
-    elif re.search("Master", line):
-        return "Master"
-    else:
-        return "Other"
-
-
-def drop_na_inputs(*, input_data: pd.DataFrame) -> pd.DataFrame:
-    """Check model inputs for na values and filter."""
-    validated_data = input_data.copy()
-    new_vars_with_na = [
-        var
-        for var in config.ml_model_config.features
-        if var not in config.ml_model_config.categorical_vars_with_na + config.ml_model_config.numerical_vars_with_na
-        and validated_data[var].isnull().sum() > 0
-    ]
-    validated_data.dropna(subset=new_vars_with_na, inplace=True)
-
-    return validated_data
 
 
 def validate_inputs(*, input_data: pd.DataFrame) -> Tuple[pd.DataFrame, Optional[dict]]:
     """Check model inputs for unprocessable values."""
-    input_data["Cabin"] = input_data["Cabin"].apply(get_first_cabin)
-    input_data["Title"] = input_data["Name"].apply(get_title)
-    # cast numerical variables as floats
-    input_data["Fare"] = input_data["Fare"].astype("float")
-    input_data["Age"] = input_data["Age"].astype("float")
 
-    input_data.drop(labels=config.ml_model_config.variables_to_drop, axis=1, inplace=True)
-
-    # Columns should coinside with config.model_config.feature
     assert input_data.columns.tolist() == config.ml_model_config.features
 
     relevant_data = input_data[config.ml_model_config.features].copy()
-    validated_data = drop_na_inputs(input_data=relevant_data)
 
     errors = None
 
     try:
         # replace numpy nans so that pydantic can validate
-        MultipleTitanicInputs(inputs=validated_data.replace({np.nan: None}).to_dict(orient="records"))
+        MultipleAudioInputs(inputs=relevant_data.replace({np.nan: None}).to_dict(orient="records"))
     except ValidationError as error:
         errors = error.json()
 
-    return validated_data, errors
+    return relevant_data, errors
 
 
-class TitanicInputSchema(BaseModel):
-    PassengerId: Optional[int]
-    Pclass: Optional[int]
-    Name: Optional[str]
-    Sex: Optional[str]
-    Age: Optional[float]
-    SibSp: Optional[int]
-    Parch: Optional[int]
-    Ticket: Optional[str]
-    Fare: Optional[float]
-    Cabin: Optional[str]
-    Embarked: Optional[str]
+class AudioInputSchema(BaseModel):
+    chroma_stft_mean: float
+    chroma_stft_var: float
+    rms_mean: float
+    rms_var: float
+    spectral_centroid_mean: float
+    spectral_centroid_var: float
+    spectral_bandwidth_mean: float
+    spectral_bandwidth_var: float
+    rolloff_mean: float
+    rolloff_var: float
+    zero_crossing_rate_mean: float
+    zero_crossing_rate_var: float
+    harmony_mean: float
+    harmony_var: float
+    tempo: float
+    mfcc1_mean: float
+    mfcc1_var: float
+    mfcc2_mean: float
+    mfcc2_var: float
+    mfcc3_mean: float
+    mfcc3_var: float
+    mfcc4_mean: float
+    mfcc4_var: float
+    mfcc5_mean: float
+    mfcc5_var: float
+    mfcc6_mean: float
+    mfcc6_var: float
+    mfcc7_mean: float
+    mfcc7_var: float
+    mfcc8_mean: float
+    mfcc8_var: float
+    mfcc9_mean: float
+    mfcc9_var: float
+    mfcc10_mean: float
+    mfcc10_var: float
+    mfcc11_mean: float
+    mfcc11_var: float
+    mfcc12_mean: float
+    mfcc12_var: float
+    mfcc13_mean: float
+    mfcc13_var: float
+    mfcc14_mean: float
+    mfcc14_var: float
+    mfcc15_mean: float
+    mfcc15_var: float
+    mfcc16_mean: float
+    mfcc16_var: float
+    mfcc17_mean: float
+    mfcc17_var: float
+    mfcc18_mean: float
+    mfcc18_var: float
+    mfcc19_mean: float
+    mfcc19_var: float
+    mfcc20_mean: float
+    mfcc20_var: float
 
-
-class MultipleTitanicInputs(BaseModel):
-    inputs: List[TitanicInputSchema]
+class MultipleAudioInputs(BaseModel):
+    inputs: List[AudioInputSchema]
