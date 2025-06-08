5ed831f0548d904ff6adc2d5ecd5ee6cccad715f
Petr Doroshev <doroshevpetr@gmail.com>
2025-06-08 10:29:25 +0300

    predict tests updated

--- tests/test_prediction.py
+++ tests/test_prediction.py
@@ -9,3 +9,11 @@ def test_make_prediction(input_data):
     assert isinstance(results, dict)
     assert errors is None or isinstance(errors, dict)
 
+def test_prediction_structure(input_data):
+
+    results, errors = make_prediction(input_data=input_data)
+    assert isinstance(results["preds"], list)
+    assert isinstance(results["probs"], list)
+    assert isinstance(results["version"], str)
+    assert errors is None
+
